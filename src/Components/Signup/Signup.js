e8a53e99a549f18874620fc317dc20d729aa1b7a
Suras Kariyil <suraskariyil@gmail.com>
2024-01-08 14:29:53 +0530

    form submition part started

--- src/Components/Signup/Signup.js
+++ src/Components/Signup/Signup.js
@@ -1,19 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import Logo from '../../olx-logo.png';
 import './Signup.css';
 
 export default function Signup() {
+  const [username,setUsername]=useState[''];
+  const [email,setEmail]=useState[''];
+  const [phone,setPhone]=useState[''];
+  const [password,setPassword]=useState[''];
+
+  const handleSubmit =(e)=>{
+    e.preventDefault()
+    console.log(username);
+    }
+
   return (
     <div>
       <div className="signupParentDiv">
         <img width="200px" height="200px" src={Logo}></img>
-        <form>
+        <form onSubmit={handleSubmit}> 
           <label htmlFor="fname">Username</label>
           <br />
           <input
             className="input"
             type="text"
+            value={username}
+            onChange={(e)=>setUsername(e.target.value)}
             id="fname"
             name="name"
             defaultValue="John"
@@ -24,6 +36,8 @@ export default function Signup() {
           <input
             className="input"
             type="email"
+            value={email}
+            onChange={(e)=>setEmail(e.target.value)}
             id="fname"
             name="email"
             defaultValue="John"
@@ -34,6 +48,8 @@ export default function Signup() {
           <input
             className="input"
             type="number"
+            value={phone}
+            onChange={(e)=>setPhone(e.target.value)}
             id="lname"
             name="phone"
             defaultValue="Doe"
@@ -44,6 +60,8 @@ export default function Signup() {
           <input
             className="input"
             type="password"
+            value={password}
+            onChange={(e)=>setPassword(e.target.value)}
             id="lname"
             name="password"
             defaultValue="Doe"
